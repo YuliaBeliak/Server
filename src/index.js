b70c74a346c49b3415e319899fcb34fa82a121d2
YuliaBeliak <belyakzhe@gmail.com>
2019-11-12 18:35:01 +0300

    feat: add validation to user-service

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');;
+const bodyParser = require('body-parser');
 const router = require('./routers/user');
 
 const app = express();
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -13,45 +13,61 @@ const updateStorage = () => {
 const generateId = () => `f${(~~(Math.random() * 1e8)).toString(16)}`;
 
 const add = req => {
-    const newUser = req.body;
-    newUser.id = generateId();
-    storage.push(newUser);
-    updateStorage();
-    return newUser;
+    if (!req.body.firstName || !req.body.lastName) {
+        throw new Error('Incorrect data, json should contain "firstName" and "lastName" fields');
+    } else {
+        const newUser = req.body;
+        newUser.id = generateId();
+        storage.push(newUser);
+        updateStorage();
+        return `New user ${newUser.firstName} ${newUser.lastName} has been added`;
+    }
 };
 
 const get = req => {
-    const {id} = req.body;
-    const idx = storage.findIndex(el => el.id === id);
-    if (idx > 0) {
-        return storage[idx];
+    if (!req.body.id) {
+        throw new Error('Incorrect data, json should contain "id" field');
     } else {
-        throw new Error('User not found');
+        const {id} = req.body;
+        const idx = storage.findIndex(el => el.id === id);
+        if (idx >= 0) {
+            return storage[idx];
+        } else {
+            throw new Error('User not found');
+        }
     }
 };
 
 const update = req => {
-    const user = req.body;
-    const {id} = user;
-    const idx = storage.findIndex(el => el.id === id);
-    if (idx > 0) {
-        storage[idx] = user;
-        updateStorage();
-        return ('User data has been updated')
+    if (!req.body.id || !req.body.firstName || !req.body.lastName) {
+        throw new Error('Incorrect data, json should contain "id", "firstName", "lastName" fields');
     } else {
-        throw new Error('User not found');
+        const user = req.body;
+        const {id} = user;
+        const idx = storage.findIndex(el => el.id === id);
+        if (idx >= 0) {
+            storage[idx] = user;
+            updateStorage();
+            return 'User data has been updated';
+        } else {
+            throw new Error('User not found');
+        }
     }
 };
 
 const remove = req => {
-    const {id} = req.body;
-    const idx = storage.findIndex(el => el.id === id);
-    if (idx > 0) {
-        storage.splice(idx, 1);
-        updateStorage();
-        return ('User has been removed');
+    if (!req.body.id) {
+        throw new Error('Incorrect data, json should contain "id" field');
     } else {
-        throw new Error('User not found');
+        const {id} = req.body;
+        const idx = storage.findIndex(el => el.id === id);
+        if (idx >= 0) {
+            storage.splice(idx, 1);
+            updateStorage();
+            return ('User has been removed');
+        } else {
+            throw new Error('User not found');
+        }
     }
 };
 
